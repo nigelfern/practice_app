843712283d9171d1145b9112a2b0f84beaffbf8f
nigelfern <55825785+nigelfern@users.noreply.github.com>
2021-01-28 20:03:30 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+    </header>
+  </div>
+}
+export default App;
